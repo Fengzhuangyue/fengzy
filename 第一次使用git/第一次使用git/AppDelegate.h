72203604b2bf75759a1a2c1bf135289fa88db275
冯庄玥 <fengzhuangyue@promote.cache-dns.local>
2016-02-26 12:38:03 +0800

    初始化远程项目

---
+++ 第一次使用git/第一次使用git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  第一次使用git
+//
+//  Created by 冯庄玥 on 16/2/26.
+//  Copyright © 2016年 冯庄玥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
